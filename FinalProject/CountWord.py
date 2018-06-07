023f4d1620ed9977779a25e75b5cb830452cf960
tornadoblot <newclearplant@gmail.com>
2018-06-07 18:07:26 +0900

    num1 draw line

--- FinalProject/CountWord.py
+++ FinalProject/CountWord.py
@@ -6,36 +6,65 @@ import sys
 # 오류 발생 시 실행하는 함수
 def isExcept():
     while True:
-        n = int(input("""1: 파일명 재입력
+        n = input("""1: 파일명 재입력
 2: 프로그램 종료
-"""))
+""")
         # 파일명을 재 입력하고 싶으면 재입력하고
-        if n == 1:
+        if n == '1':
             return True
         # 종료하고 싶으면 sys.exit()함수를 이용해 종료한다
-        elif n == 2:
+        elif n == '2':
             sys.exit()
         else:
             print("1 혹은 2 값을 입력해주세요")
+            continue
 
 
 # 그래프 창 그리기
 def drawWin(wordCnt):
     win = GraphWin("CountWord", 1000, 500)
-    win.setBackground("white")
+    win.setBackground("lavenderblush")
     win.setCoords(-50, 550, 1050, -50)
 
-    for i in range(8):
-        x = 50 + 120 * i
-        Text(Point(x, 450), wordCnt[i][0]).draw(win)
+    mTxt = Text(Point(500, -10), "그래프창 활성화 후 아무키나 누르면 종료합니다")
+    mTxt.setStyle("bold italic")
+    mTxt.setSize(18)
+    mTxt.setTextColor("mediumvioletred")
+    mTxt.draw(win)
+    Line(Point(0, 470), Point(0, 20)).draw(win)
 
-        bar = Rectangle(Point(x - 20, 430), Point(x + 20, 430 - 20 * wordCnt[i][1]))
-        bar.draw(win)
+    print(wordCnt)
+    for i in range(8):
+        x = 50 + 130 * i
+        Text(Point(x, 490), wordCnt[i][0]).draw(win)
+
+        if 470 - 20 * wordCnt[i][1] <= 20:
+            bar = Rectangle(Point(x - 20, 470), Point(x + 20, 20))
+            bar.setWidth(0)
+            bar.setFill("hotpink")
+            bar.draw(win)
+
+            txt = Text(Point(x, 225), str(wordCnt[i][1]))
+            txt.setTextColor("white")
+            txt.draw(win)
+
+        else:
+            bar = Rectangle(Point(x - 20, 470), Point(x + 20, 470 - 20 * wordCnt[i][1]))
+            bar.setWidth(0)
+            bar.setFill("hotpink")
+            bar.draw(win)
+
+            txt = Text(Point(x, 470 - 10 * wordCnt[i][1]), str(wordCnt[i][1]))
+            txt.setTextColor("white")
+            txt.draw(win)
+
+      
         
 
 
     if win.getKey():
         win.close()
+
 
 
 
@@ -47,7 +76,7 @@ def main():
 
             file = open(fName+'.txt', 'r')
             # spilit으로 단어 나누기
-            text = file.read().split()
+            text = file.read().lower().split()
 
         # 파일이 존재하지 않는 오류가 발생하면
         except FileNotFoundError:
@@ -71,9 +100,8 @@ def main():
     wList = sorted(wList.items(), key = lambda x: x[0])
     wList = sorted(wList, key = lambda x: x[1], reverse = True)
 
-    print(wList)
+    drawWin(sorted(wList[:8], key = lambda x: x[0]))
 
-    drawWin(wList)
     file.close()
 
 main()
