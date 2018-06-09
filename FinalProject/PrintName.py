42dcaf0ea72761009dba1a57967ec2e69866180e
Tornadoblot <newclearplant@gmail.com>
2018-06-09 19:01:07 +0900

    print name
    어제 까먹고 커밋을 못한것이여ㅎ; 1이면 '*' 출력하는거까지 하고 잠

--- FinalProject/PrintName.py
+++ FinalProject/PrintName.py
@@ -1,2 +1,23 @@
 # 프로그래밍 입문 기말프로젝트 #2
 
+f = open("name.txt", 'r')
+
+line = f.read().splitlines()
+cnt = 0
+
+#if line[0][9] == '1':
+#    print('*')
+
+for i in range(len(line)):
+    for j in range(len(line[i])):
+        if line[i][j] == '1':
+            print("*", end='')
+        else:
+            print(" ", end='')
+
+    #cnt += 1
+    #print(line[i][j])
+    print()
+    
+            
+f.close()
